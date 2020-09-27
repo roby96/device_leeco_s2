a9edee833199de2b309054023068df2905005516
LuK1337 <priv.luk@gmail.com>
2020-09-27 17:45:33 +0200

    s2: Use libcutils-v29.so for libdpmframework.so
    * v30 one doesn't have `strdup8to16` symbol in it.
    
    Change-Id: Id2b4bb3017bc24c817dbe4b03834960aa1a4b444

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,11 @@ function blob_fixup() {
         "${PATCHELF}" --remove-needed "libkeymaster_messages.so" "${2}"
         ;;
 
+    # Use libcutils-v29.so for libdpmframework.so
+    product/lib64/libdpmframework.so)
+        sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
+        ;;
+
     esac
 }
 
