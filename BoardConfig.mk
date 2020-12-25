a4d25dfad469c57100904c84d82a3fefc29c3c67
Michael Bestas <mkbestas@lineageos.org>
2020-12-25 03:15:08 +0200

    s2: Set androidboot.init_fatal_reboot_target=recovery
    * Don't crash to bootloader, users can access pstore in
      recovery in order to debug the issue.
    
    Change-Id: I6025b6af74e8052918c101236a46cb512a888695

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8952
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlyprintk
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x00000100
