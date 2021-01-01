45020ffa1f0c727f05dafef0e4e032ebf68dcc25
Bruno Martins <bgcngm@gmail.com>
2021-01-01 00:27:52 +0000

    s2: fingerprint: Include VINTF fragment
    Change-Id: I48ce57982e818ce06b4c57b5df700d8a4acf4284

--- fingerprint/gdxbiometrics/Android.mk
+++ fingerprint/gdxbiometrics/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.leeco_s2
 LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.leeco_s2.rc
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_VINTF_FRAGMENTS := android.hardware.biometrics.fingerprint@2.1-service.leeco_s2.xml
 LOCAL_SRC_FILES := \
     BiometricsFingerprint.cpp \
     service.cpp \
